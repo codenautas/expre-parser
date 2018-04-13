76da1f9b341ed75e566bc85550fc33a0d1b0f7eb
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-04-13 15:53:00 -0300

    agrega compilación a postgres usando distpaching dinámico

--- src/expre-parser.ts
+++ src/expre-parser.ts
@@ -1,18 +1,77 @@
 "use strict";
 
-import * as sqliteParser from "sqlite-parser";
-import { ParsedObject, BaseNode as SPBaseNode } from "sqlite-parser";
-
 // https://github.com/codeschool/sqlite-parser/blob/master/src/parser.js (line 1750 aprox)
 // also see https://www.sqlite.org/datatype3.html
 
-export class BaseNode {
+import * as sqliteParser from "sqlite-parser";
+import { ParsedObject, BaseNode as SPBaseNode } from "sqlite-parser";
+
+export interface CompilerOptions {
+    varWrapper?: string | null
+    divWrapper?: string | null
+}
+
+// ########## API class #######
+export class ExpresionParser {
+
+    static compileToPostgres(expression: string, opts: CompilerOptions, pkExpression: string): string {
+        return (new Compiler(opts)).toPostgres(ExpresionParser.parse(expression), pkExpression);
+    }
+
+    static parse(expression: string): BaseNode {
+        let ast: ParsedObject = sqliteParser(expression);
+        var astStatement: SPBaseNode = ast.statement[0].result[0];
+        let node = ExpresionParser.convertNode(astStatement);
+        return node;
+    }
+
+    static convertNode(ast: SPBaseNode): BaseNode {
+        var resultNode: BaseNode;
+        // uso switch case porque en sqliteParser.d.ts solo tengo los tipos pero no comportamiento orientado a objetos
+        switch (ast.type) {
+            case 'literal':
+                resultNode = new LiteralNode(<sqliteParser.LiteralNode>ast)
+                break;
+            case 'identifier':
+                resultNode = new IdentifierNode(<sqliteParser.IdentifierNode>ast)
+                break;
+            case 'function':
+                resultNode = new FunctionExpressionNode(<sqliteParser.FunctionNode>ast);
+                break;
+            case 'expression':
+                switch ((ast as sqliteParser.ExpressionNode).format) {
+                    case 'binary':
+                        resultNode = new BinaryExpressionNode(<sqliteParser.BinaryExpressionNode>ast)
+                        break;
+                    case 'unary':
+                        resultNode = new UnaryExpressionNode(<sqliteParser.UnaryExpressionNode>ast)
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+        return resultNode;
+    }
+}
+
+export interface CompileDelegation {
+    toPostgres(c: Compiler): any;
+}
+
+export abstract class BaseNode implements CompileDelegation {
     constructor(public type: string, public mainContent: string) {
     }
+    abstract toPostgres(c: Compiler): any 
 }
 export class IdentifierNode extends BaseNode {
     constructor(ast: sqliteParser.IdentifierNode) {
         super('identifier', ast.name)
+    }
+    toPostgres(c: Compiler): string {
+        return c.identifierToPostgres(this)
     }
 }
 export class LiteralNode extends BaseNode {
@@ -21,86 +80,96 @@ export class LiteralNode extends BaseNode {
         super('literal', ast.value)
         this.dataType = ast.variant
     }
+    toPostgres(c: Compiler): any {
+        return c.literalToPostgres(this)
+    }
 }
-export class ExpressionNode extends BaseNode {
+export abstract class ExpressionNode extends BaseNode {
     children: BaseNode[];
     constructor(type: string = 'expression', mainContent: string, children: sqliteParser.BaseNode[], ) {
         super(type, mainContent);
-        this.children = children.map(exp => processTree(exp))
+        this.children = children.map(exp => ExpresionParser.convertNode(exp))
     }
+    abstract toPostgres(c: Compiler): any
 }
 export class BinaryExpressionNode extends ExpressionNode {
     constructor(ast: sqliteParser.BinaryExpressionNode) {
         super('binary', ast.operation, [ast.left, ast.right])
+    }
+    toPostgres(c: Compiler): any {
+        return c.binaryToPostgres(this)
     }
 }
 export class UnaryExpressionNode extends ExpressionNode {
     constructor(ast: sqliteParser.UnaryExpressionNode) {
         super('unary', ast.operator, [ast.expression])
     }
+    toPostgres(c: Compiler): any {
+        return c.unaryToPostgres(this)
+    }
 }
 export class FunctionExpressionNode extends ExpressionNode {
     constructor(ast: sqliteParser.FunctionNode) {
         super('function', ast.name.name, ast.args.expression)
     }
-}
-
-function processTree(ast: SPBaseNode): BaseNode {
-    var returnValue: BaseNode;
-    switch (ast.type) {
-        case 'literal':
-            returnValue = new LiteralNode(ast as sqliteParser.LiteralNode)
-            break;
-        case 'identifier':
-            returnValue = new IdentifierNode(<sqliteParser.IdentifierNode>ast)
-            break;
-        case 'function':
-            returnValue = new FunctionExpressionNode(<sqliteParser.FunctionNode>ast);
-            break;
-        case 'expression':
-            switch ((ast as sqliteParser.ExpressionNode).format) {
-                case 'binary':
-                    returnValue = new BinaryExpressionNode(ast as sqliteParser.BinaryExpressionNode)
-                    break;
-                case 'unary':
-                    returnValue = new UnaryExpressionNode(ast as sqliteParser.UnaryExpressionNode)
-                    break;
-                default:
-                    break;
-            }
-            break;
-        default:
-            break;
+    toPostgres(c: Compiler): any {
+        return c.functionToPostgres(this)
     }
-    return returnValue;
-}
-
-export interface CompilerOptions {
-    varWrapper?: string | null
-    divWrapper?: string | null
 }
 
 export class Compiler {
-    constructor(private compilerOptions: CompilerOptions) {
+
+    protected pkExpression: string;
+    constructor(public options: CompilerOptions) {
     }
+
     toPostgres(node: BaseNode, pkExpression: string): string {
-        return 'adsf'
-        // if (node.type)
-
-        //     return resultStr;
+        this.pkExpression = pkExpression;
+        return node.toPostgres(this);
     }
-    toJavascript(tree: BaseNode, pkExpression: string): string {
-        return "not implemented yet"
+
+    toJavascript(node: BaseNode): string {
+        return node.toJavascript();
     }
-}
 
-export function compiler(compilerOptions: CompilerOptions): Compiler {
-    return new Compiler(compilerOptions);
-}
+    functionToPostgres(funcNode: FunctionExpressionNode): any {
+        return this.baseToPostgres(funcNode) + '(' + funcNode.children.map(child => child.toPostgres(this)).join(', ') + ')';
+    }
+    unaryToPostgres(unaryNode: UnaryExpressionNode): any {
+        return this.baseToPostgres(unaryNode) + ' ' + unaryNode.children[0].toPostgres(this)
+    }
+    binaryToPostgres(binNode: BinaryExpressionNode): any {
+        let left = binNode.children[0].toPostgres(this);
+        let right = binNode.children[1].toPostgres(this)
+        if (binNode.mainContent == '/') {
+            return `div(${left}, ${right})`;// return 'div(' + left + ',' + right + ')';
+        } else {
+            return left + ' ' + this.baseToPostgres(binNode) + ' ' + right
+        }
+    }
+    literalToPostgres(litNode: LiteralNode): string | number {
+        let resultVal: string | number;
+        switch (litNode.dataType) {
+            case 'text':
+                resultVal = "'" + this.baseToPostgres(litNode) + "'"
+                break;
+            case 'decimal':
+                resultVal = parseFloat(this.baseToPostgres(litNode))
+                break;
+            case 'null':
+                resultVal = this.baseToPostgres(litNode)
+                break;
+            default:
+                break;
+        }
+        return resultVal;
+    }
 
-export function parse(expression: string): BaseNode {
-    let ast: ParsedObject = sqliteParser(expression);
-    var astStatement: SPBaseNode = ast.statement[0].result[0];
-    let node = processTree(astStatement);
-    return node;
+    identifierToPostgres(idNode: IdentifierNode): string {
+        return !this.options.varWrapper ? this.baseToPostgres(idNode) : this.options.varWrapper + '(' + this.baseToPostgres(idNode) + ')'
+    }
+
+    baseToPostgres(node: BaseNode): string {
+        return node.mainContent;
+    }
 }
--- test/test-simples.ts
+++ test/test-simples.ts
@@ -4,11 +4,12 @@ import * as discrepances from 'discrepances';
 import 'mocha';
 
 import * as EP from '../src/expre-parser';
+import {ExpresionParser, Compiler} from '../src/expre-parser';
 import * as sqliteParser from 'sqlite-parser';
 
-function compare<T>(obtained: T, expected: T) {
+function compare<T>(obtained: T, expected: T, opts?: object) {
     try {
-        discrepances.showAndThrow(obtained, expected);
+        discrepances.showAndThrow(obtained, expected, opts);
     } catch (err) {
         console.log('ERROR FORMATO 1');
         console.log(JSON.stringify(discrepances.nestedObject(obtained, expected)))
@@ -21,130 +22,83 @@ function compare<T>(obtained: T, expected: T) {
 describe("expre-parser", function () {
     describe("parser", function () {
         it("parse one number", async function () {
-            let obtained = EP.parse("select 43") as EP.LiteralNode;
-            let entry:sqliteParser.LiteralNode = {type: 'literal', value: "43", variant:'decimal'}
-            let expected = new EP.LiteralNode(entry)
-            compare(obtained.type, 'literal')
-            compare(obtained.mainContent, entry.value)
-            compare(obtained.dataType, entry.variant)
-            //Se podria usar flaten en lugar de compare para comprar contenido (sin hacer todo eso)
-            compare(obtained, expected);
-            obtained = expected;
+            let obtained = ExpresionParser.parse("select 43") as EP.LiteralNode;
+            let expectedParsed = new EP.LiteralNode(<sqliteParser.LiteralNode>{type: 'literal', value: "43", variant:'decimal'})
+            // es llamado `jsonObj` en lugar de `LiteralObj` u `objLiteral` para no confundir el `literal` de objetos JS con nuestro LiteralNode de AST
+            let expectedJsonObj = <EP.LiteralNode> {type:'literal',dataType:'decimal',mainContent:"43"};
+            compare(obtained, expectedJsonObj, {duckTyping: true });
+            compare(obtained, expectedParsed);
+            obtained = expectedParsed; //assignation to TS type check            
         });
         it("parse one text", async function () {
-            let obtained = EP.parse("select 'hello world'") as EP.LiteralNode;
-            let entry:sqliteParser.LiteralNode = {type: 'literal', value: "hello world", variant:'text'}
-            let expected = new EP.LiteralNode(entry)
-            compare(obtained.type, 'literal')
-            compare(obtained.mainContent, entry.value)
-            compare(obtained.dataType, entry.variant)
-            compare(obtained, expected);
-            obtained = expected;
+            let obtained = ExpresionParser.parse("select 'hello world'") as EP.LiteralNode;
+            let expectedParsed = new EP.LiteralNode({type: 'literal', value: "hello world", variant:'text'})
+            let expectedJsonObj = <EP.LiteralNode> {type: 'literal', dataType:'text', mainContent:'hello world'}
+            compare(obtained, expectedJsonObj, {duckTyping: true });
+            compare(obtained, expectedParsed);
+            obtained = expectedParsed; //assignation to TS type check   
         });
         it("parse one variable", async function () {
-            let obtained = EP.parse("select t.c")  as EP.IdentifierNode;
-            let entry:sqliteParser.IdentifierNode = { type: 'identifier', name: "t.c" }
-            let expected = new EP.IdentifierNode(entry);
-            compare(obtained.type, 'identifier')
-            compare(obtained.mainContent, entry.name)
-            compare(obtained, expected);
-            obtained = expected;
+            let obtained = ExpresionParser.parse("select t.c")  as EP.IdentifierNode;
+            let expectedParsed = new EP.IdentifierNode({ type: 'identifier', name: "t.c" });
+            let expectedJsonObj = <EP.IdentifierNode> {type: 'identifier', mainContent:'t.c'}
+            compare(obtained, expectedJsonObj, {duckTyping: true });
+            compare(obtained, expectedParsed);
+            obtained = expectedParsed; //assignation to TS type check   
         });
         it("parse one function", async function () {
-            let obtained = EP.parse("select f(33)") as EP.FunctionExpressionNode;
-            let entry: sqliteParser.FunctionNode = { type: 'function', name: {type: 'identifier', name: "f"}, args: { expression: [<sqliteParser.LiteralNode>{ type: 'literal', value: '33', variant: 'decimal' }]} };
-            let expected = new EP.FunctionExpressionNode(entry);
-            compare(obtained.type, 'function')
-            compare(obtained.mainContent, entry.name.name)
-            compare(obtained.children.length, 1)
-            compare(obtained, expected);
-            obtained = expected;
+            let obtained = ExpresionParser.parse("select f(33)") as EP.FunctionExpressionNode;
+            let expectedParsed = new EP.FunctionExpressionNode({ type: 'function', name: {type: 'identifier', name: "f"}, args: { expression: [<sqliteParser.LiteralNode>{ type: 'literal', value: '33', variant: 'decimal' }]} });
+            let expectedJsonObj: object = {type:"function",mainContent:"f", children:[{type:"literal",mainContent:"33",dataType:"decimal"}]}
+            compare(obtained, expectedJsonObj, {duckTyping: true });
+            compare(obtained, expectedParsed);
+            obtained = expectedParsed; //assignation to TS type check   
         });
         it("parse one addition", async function () {
-            let obtained = EP.parse("select 5+4") as EP.BinaryExpressionNode;
-            let entry: sqliteParser.BinaryExpressionNode = {
+            let obtained = ExpresionParser.parse("select 5+4") as EP.BinaryExpressionNode;
+            let expectedParsed = new EP.BinaryExpressionNode({
                 type: "expression",
                 format: 'binary',
                 operation: "+",
                 left: <sqliteParser.LiteralNode>{ type: "literal", value: "5", variant: "decimal" },
                 right: <sqliteParser.LiteralNode>{ type: "literal", value: "4", variant: "decimal" }
-            }
-            let expected = new EP.BinaryExpressionNode(entry)
-            compare(obtained.type, 'binary')
-            compare(obtained.mainContent, '+')
-            compare(obtained.children.length, 2)
-            compare(obtained.children[0].type, 'literal')
-            compare(obtained.children[1].mainContent, '4')
-            compare(obtained, expected);
-            obtained = expected;
+            })
+            let expectedJsonObj: object = {type:"binary",mainContent:"+",children:[{type:"literal",mainContent:"5",dataType:"decimal"},{type:"literal",mainContent:"4",dataType:"decimal"}]}
+            compare(obtained, expectedJsonObj, {duckTyping: true });
+            compare(obtained, expectedParsed);
+            obtained = expectedParsed; //assignation to TS type check   
         });
         it("parse one complete", async function () {
-            let obtained = EP.parse("select 'a'+'b' = 6 AND fun(a,b,c) AND not f(3) OR 6/2 is 3");
-            let expected = {
-                type: "binary",
-                mainContent: "or",
-                children: [
-                    {
-                        type: "binary", mainContent: "and", children: [
-                            {
-                                type: "binary", mainContent: "and", children: [
-                                    {
-                                        type: "binary", mainContent: "=", children: [
-                                            {
-                                                type: "binary", mainContent: "+", children: [
-                                                    { type: "literal", mainContent: "a", dataType: "text" },
-                                                    { type: "literal", mainContent: "b", dataType: "text" }
-                                                ]
-                                            },
-                                            { type: "literal", mainContent: "6", dataType: "decimal" }
-                                        ]
-                                    },
-                                    {
-                                        type: "function", mainContent: "fun", children: [
-                                            { type: "identifier", mainContent: "a" },
-                                            { type: "identifier", mainContent: "b" },
-                                            { type: "identifier", mainContent: "c" }]
-                                    }]
-                            },
-                            {
-                                type: "unary", mainContent:"not", children: [
-                                    {
-                                        type: "function", mainContent: "f", children: [
-                                            { type: "literal", mainContent: "3", dataType: "decimal" }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        type: "binary", mainContent: "is", children: [
-                            {
-                                type: "binary", mainContent: "/", children: [
-                                    { type: "literal", mainContent: "6", dataType: "decimal" },
-                                    { type: "literal", mainContent: "2", dataType: "decimal" }
-                                ]
-                            },
-                            { type: "literal", mainContent: "3", dataType: "decimal" }
-                        ]
-                    }]
-            }
-            discrepances.flatten(obtained, expected);
-            obtained = expected;
+            let obtained = ExpresionParser.parse("select 'a'+'b' = 6 AND fun(a,b,c) AND not f(3) OR 6/2 is 3") as EP.BinaryExpressionNode;
+            // let expectedParsed = new EP.BinaryExpressionNode(<sqliteParser.BinaryExpressionNode>{put here the json in format sqliteParser.BinaryExpressionNode});
+            let expectedJsonObj: object = {type:"binary",mainContent:"or",children:[{type:"binary",mainContent:"and",children:[{type:"binary",mainContent:"and",children:[{type:"binary",mainContent:"=",children:[{type:"binary",mainContent:"+",children:[{type:"literal",mainContent:"a",dataType:"text"},{type:"literal",mainContent:"b",dataType:"text"}]},{type:"literal",mainContent:"6",dataType:"decimal"}]},{type:"function",mainContent:"fun",children:[{type:"identifier",mainContent:"a"},{type:"identifier",mainContent:"b"},{type:"identifier",mainContent:"c"}]}]},{type:"unary",mainContent:"not",children:[{type:"function",mainContent:"f",children:[{type:"literal",mainContent:"3",dataType:"decimal"}]}]}]},{type:"binary",mainContent:"is",children:[{type:"binary",mainContent:"/",children:[{type:"literal",mainContent:"6",dataType:"decimal"},{type:"literal",mainContent:"2",dataType:"decimal"}]},{type:"literal",mainContent:"3",dataType:"decimal"}]}]}
+            compare(obtained, expectedJsonObj, {duckTyping: true });
+            // // compare(obtained, expectedParsed);
+            // obtained = expectedParsed; //assignation to TS type check   
         });
     });
     describe("wrapped", function () {
-        var compilerOptions = {
+        var compilerOptions: EP.CompilerOptions = {
             varWrapper: 'var',
             divWrapper: 'div'
         }
-        var compiler: EP.Compiler;
+        var compiler: Compiler;
         before(function () {
-            compiler = EP.compiler(compilerOptions);
+            compiler = new Compiler(compilerOptions);
         });
         it('wrap varname', function () {
-            let obtained = compiler.toPostgres(EP.parse("f(t.field)"), 'pk1');
-            let expected = "f(var(t.field))"
+            let obtained = compiler.toPostgres(ExpresionParser.parse("select f(t.field, 33, 'aa', b, '99')"), 'pk1');
+            let expected = "f(var(t.field), 33, 'aa', var(b), '99')"
+            compare(obtained, expected);
+        });
+        it('wrap simple division', function () {
+            let obtained = compiler.toPostgres(ExpresionParser.parse("select 7/4"), 'pk1');
+            let expected = "div(7, 4)"
+            compare(obtained, expected);
+        });
+        it('wrap division', function () {
+            let obtained = compiler.toPostgres(ExpresionParser.parse("select a/4 > 3 AND t.field/b = 8 OR 3/0 = 1 AND 0/4 < 2"), 'pk1');
+            let expected = "div(var(a), 4) > 3 and div(var(t.field), var(b)) = 8 or div(3, 0) = 1 and div(0, 4) < 2"
             compare(obtained, expected);
         });
     });
@@ -153,18 +107,18 @@ describe("expre-parser", function () {
             varWrapper: null,
             divWrapper: null
         }
-        var compiler: EP.Compiler;
+        var compiler: Compiler;
         before(function () {
-            compiler = EP.compiler(compilerOptions);
+            compiler = new Compiler(compilerOptions);
         });
-        it.skip('simple expressions toPostgres', function () {
-            let obtained = compiler.toPostgres(EP.parse("a+b>c and b is null"), 'pk1');
-            let expected = "a+b>c and b is null"
+        it('simple expressions toPostgres', function () {
+            let obtained = compiler.toPostgres(ExpresionParser.parse("select a+b>c and b is null"), 'pk1');
+            let expected = "a + b > c and b is null"
             compare(obtained, expected);
         });
         it.skip('simple expressions toJavascript', function () {
-            let obtained = compiler.toJavascript(EP.parse("a+b>c and b is null"), 'pk1');
-            let expected = "a+b>c && b == null"
+            let obtained = compiler.toJavascript(ExpresionParser.parse("select (a+b)>c AND b is null OR not t.c"));
+            let expected = "(a + b) > c && b == null || ! t.c"
             compare(obtained, expected);
         });
     });
